5ee9a68e81b48e0c817b2dda03f7f6b07409dfce
Gage Hilyard <gagehilyard@gmail.com>
2023-11-11 14:55:44 -0700

    complete

--- app/SearchBar/SearchBar.tsx
+++ app/SearchBar/SearchBar.tsx
@@ -1,15 +1,39 @@
 import { useState, useEffect } from 'react'
+import './SearchBar.css'
 
 export default function SearchBar() {
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [searchString, setSearchString] = useState('')
+    useEffect(fetchAPIdata, [searchString])
     function fetchAPIdata() {
         fetch('https://fakestoreapi.com/products')
             .then((response) => response.json())
-            .then((result) => console.log(result))
+            .then((products) => {
+                const filtered = products.filter((el) => {
+                    return el.title
+                        .toLowerCase()
+                        .includes(searchString.toLowerCase())
+                })
+                const currentFilter = filtered.map((product, index) => {
+                    return (
+                        <div className="product">
+                            <p className="title">{product.title}</p>
+                            <img src={product.image} />
+                            <p className="price">${product.price}</p>
+                        </div>
+                    )
+                })
+                setFilteredProducts(currentFilter)
+            })
     }
-    useEffect(fetchAPIdata, [])
+    function handleChange(event) {
+        setSearchString(event.target.value)
+    }
     return (
         <div>
-            <p>Here</p>
+            <p>The Search Bar</p>
+            <input type="text" value={searchString} onChange={handleChange} />
+            <div id="container">{filteredProducts}</div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 'use client'
 import Counter from './Counter/counter.tsx'
 import SearchBar from './SearchBar/SearchBar.tsx'
+
+export default function Page() {
+    return (
+        <main>
+            <Counter />
+            <hr />
+            <a href="https://github.com/Oxmoon/nextjs-demo">My Github Link</a>
+            <hr />
+            <SearchBar />
+        </main>
+    )
+}
